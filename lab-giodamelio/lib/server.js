5f0d9f71a74e0b9a3ce544f8b520e16af2815cb7
Gio d'Amelio <giodamelio@gmail.com>
2016-07-26 21:17:40 -0700

    Add morgan logger

--- lab-giodamelio/lib/server.js
+++ lab-giodamelio/lib/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const server = express();
 
 server.use(bodyParser.json());
+server.use(morgan('dev'));
 server.use('/api/pokemon', require('./routes/pokemon'));
 
 module.exports = server;
