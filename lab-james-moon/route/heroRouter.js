92bb8f6e0d543f93196639d1acaabf604cba6d23
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-07-28 11:53:13 -0700

    saves

--- lab-james-moon/route/heroRouter.js
+++ lab-james-moon/route/heroRouter.js
@@ -1,7 +1,22 @@
 'use strict';
 const express = require('express');
+const mongoose = require('mongoose');
 const heroRouter = express.Router();
+const jsonParser = require('body-parser').json();
+const Hero = require('../model/hero');
 
-heroRouter.get('/', (req, res) => {
-  console.log('entering the hero router');
+const LOCAL_DB_SERVER = 'mongodb://localhost/dev_db';
+const DB_SERVER = process.env.DB_SERVER || LOCAL_DB_SERVER;
+
+mongoose.connect(DB_SERVER);
+
+heroRouter.post('/hero', jsonParser, (req, res) => {
+  console.log('creating new hero');
+  let newHero = Hero(req.body);
+  newHero.save((err, hero) => {
+    if (err) return err;
+    res.json(hero);
+  });
 });
+
+module.exports = heroRouter;
--- lab-james-moon/server.js
+++ lab-james-moon/server.js
@@ -2,7 +2,6 @@
 const app = require('express')();
 const debug = require('debug');
 const serverErrLog = debug('hero:errors');
-const bodyParser = require('body-parser').json();
 const heroRouter = require('./route/heroRouter');
 
 app.use('/api', heroRouter);
