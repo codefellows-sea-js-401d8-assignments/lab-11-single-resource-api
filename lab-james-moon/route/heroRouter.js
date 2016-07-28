b98ce6d5a2837be0f7e09284aaf8f80cc8919f21
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-07-28 11:54:04 -0700

    moved mongoose connect to server file

--- lab-james-moon/route/heroRouter.js
+++ lab-james-moon/route/heroRouter.js
@@ -1,14 +1,8 @@
 'use strict';
 const express = require('express');
-const mongoose = require('mongoose');
 const heroRouter = express.Router();
 const jsonParser = require('body-parser').json();
 const Hero = require('../model/hero');
-
-const LOCAL_DB_SERVER = 'mongodb://localhost/dev_db';
-const DB_SERVER = process.env.DB_SERVER || LOCAL_DB_SERVER;
-
-mongoose.connect(DB_SERVER);
 
 heroRouter.post('/hero', jsonParser, (req, res) => {
   console.log('creating new hero');
--- lab-james-moon/server.js
+++ lab-james-moon/server.js
@@ -1,8 +1,14 @@
 'use strict';
 const app = require('express')();
 const debug = require('debug');
+const mongoose = require('mongoose');
 const serverErrLog = debug('hero:errors');
 const heroRouter = require('./route/heroRouter');
+
+const LOCAL_DB_SERVER = 'mongodb://localhost/dev_db';
+const DB_SERVER = process.env.DB_SERVER || LOCAL_DB_SERVER;
+
+mongoose.connect(DB_SERVER);
 
 app.use('/api', heroRouter);
 
