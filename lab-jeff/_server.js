8bafa1e26767c49a1693120cbac24554edb0bc48
Jeffrey Gebhardt <jeff@gebbey.com>
2016-07-28 12:33:42 -0700

    test

--- lab-jeff/_server.js
+++ lab-jeff/_server.js
@@ -3,10 +3,16 @@
 const pokemonRouter = require('./route/pokemonRouter');
 const bodyParser = require('body-parser');
 const express = require('express');
+let app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 const serverPort = 9000;
 
-let app = express();
+const mongoTestServer = 'mongodb://localhost/testDatabase';
+
+mongoose.connect(mongoTestServer);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: 'true'}));
@@ -23,8 +29,8 @@ app.use((err, req, res, next) => {
   if (err.status !== 404) {
     return next();
   }
-  res.status(404).send(err.message || 'Page not found...');
-  next();
+  res.status(404).json('Page not found...');
+  res.end();
 });
 
 
--- lab-jeff/test/test.js
+++ lab-jeff/test/test.js
@@ -1,21 +1,54 @@
-// 'use strict';
-//
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// chai.use(chaiHttp);
-// const expect = require('chai').expect;
-// const request = require('chai').request;
-// const serverPort = 9000;
-//
-// describe('routing', function() {
-//   require('../_server');
-//
-//   it('should handle unregistered routes', function(done) {
-//     request('localhost:' + serverPort)
-//     .get('/noob')
-//     .end(function(res){
-//       expect(res).to.have.status(404);
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const expect = require('chai').expect;
+const request = require('chai').request;
+const mongoose = require('mongoose');
+const serverPort = 9000;
+// const Pokemon = require('../model/pokemon');
+
+describe('routing', function() {
+  var server;
+  before((done) => {
+    server = require('../_server');
+    done();
+  });
+
+  after((done) => {
+    mongoose.connection.db.dropDatabase(() => {});
+    server.close();
+    done();
+  });
+  it('should handle unregistered routes', function(done) {
+    request('localhost:' + serverPort)
+    .get('/noob')
+    .end(function(res){
+      expect(res).to.have.status(404);
+      done();
+    });
+  });
+});
+
+// describe('routing with data present', () => {
+//   let testPokemon;
+//   before((done) => {
+//     testPokemon = Pokemon({
+//       pokeName: 'test Pokemon',
+//       pokeType: 'test Type'
+//     });
+//     testPokemon.save((err, pokemon) => {
+//       testPokemon = pokemon;
 //       done();
 //     });
+//   });
+//   it('should GET a single pokemon', (done) => {
+//     request('localhost:' + serverPort)
+//       .get('/api/pokemon/' + testPokemon._id)
+//       .end((res) => {
+//         expect(res).to.have.status(200);
+//         done();
+//       });
 //   });
 // });
