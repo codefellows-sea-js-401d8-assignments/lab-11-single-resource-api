dbe8ba695a205bf6f5f84997431388c9cbdf9a5f
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-28 20:29:23 -0700

    working on testing

--- lab-aliza/test/test.js
+++ lab-aliza/test/test.js
@@ -15,9 +15,18 @@ const TEST_DB_SERVER = 'mongodb://localhost/test_db';
 process.env.DB_SERVER = TEST_DB_SERVER;
 
 describe('Test CRUD ', () => {
+  let testUser;
   before((done) => {
     server = app.listen(3000, () => {
       console.log('up on 3000');
+    });
+    testUser = User({
+      name: 'aliza',
+      active: true,
+      date: 2016
+    });
+    testUser.save((err, user) => {
+      testUser = user;
       done();
     });
   });
@@ -42,36 +51,28 @@ describe('Test CRUD ', () => {
         done();
       });
   });
-});
 
-describe('Testing CRUD', () => {
-  let testUser;
-  before((done) => {
-    server = app.listen(3000, () => {
-      console.log('up on 3000');
-    });
-    testUser = User({
-      name: 'aliza',
-      active: true,
-      date: 2016
-    });
-    testUser.save((err, user) => {
-      testUser = user;
-      done();
-    });
-  });
-  after((done) => {
-    mongoose.connection.db.dropDatabase(() => {
-      server.close();
-      done();
-    });
-  });
   it('should GET a user', (done) => {
     request('localhost:3000')
       .get('/api/user/' + testUser._id)
       .end((err, res) => {
         expect(err).to.eql(null);
         expect(res.body.date).to.eql(2016);
+        done();
+      });
+  });
+
+  it('should PUT', (done) => {
+    request('localhost:3000')
+      .put('/api/user/' + testUser._id)
+      .send({
+        name: 'testname',
+        active: true,
+        date: 2016
+      })
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
         done();
       });
   });
