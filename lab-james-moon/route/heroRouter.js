e3154892d7cc86ae7df8d074a3a5f05c67ac5d0f
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-07-28 17:17:01 -0700

    server now handles errors, get, post, and put requests

--- lab-james-moon/route/heroRouter.js
+++ lab-james-moon/route/heroRouter.js
@@ -8,27 +8,48 @@ heroRouter.post('/hero', jsonParser, (req, res) => {
   console.log('creating new hero');
   let newHero = Hero(req.body);
   newHero.save((err, hero) => {
-    if (err) return err;
+    if (err) throw err;
     res.json(hero);
   });
 });
 
 heroRouter.get('/hero/:id', jsonParser, (req, res) => {
   Hero.findById(req.params.id, (err, hero) => {
-    if (err) return err;
+    if (err) throw err;
     res.json(hero);
   });
 });
 
 heroRouter.put('/hero/:id', jsonParser, (req, res) => {
-  let reqJson = req.body;
-  if (reqJson.name) {
-    Hero.findByIdAndUpdate(req.params.id, {name: reqJson.name}, (err, hero) => {
+  if (req.body.name) {
+    Hero.findByIdAndUpdate(req.params.id, {name: req.body.name}, (err, hero) => {
       if (err) throw err;
       console.log('updated name to ' + hero.name);
       res.json(hero);
     });
   }
+  if (req.body.race) {
+    Hero.findByIdAndUpdate(req.params.id, {race: req.body.race}, (err, hero) => {
+      if (err) throw err;
+      console.log('updated race to ' + hero.race);
+      res.json(hero);
+    });
+  }
+  if (req.body.faction) {
+    Hero.findbyIdAndUpdate(req.params.id, {faction: req.body.faction}, (err, hero) => {
+      if (err) throw err;
+      console.log('updated faction to ' + hero.faction);
+      res.json(hero);
+    });
+  }
+});
+
+heroRouter.delete('/hero/:id', jsonParser, (req, res) => {
+  Hero.findByIdAndRemove(req.params.id, (err) => {
+    if (err) throw err;
+    console.log('deleted ' + req.params.id);
+  });
+  res.send('removed hero');
 });
 
 module.exports = heroRouter;
