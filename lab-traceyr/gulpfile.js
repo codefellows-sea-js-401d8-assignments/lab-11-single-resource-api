2df6abfed6d292f3fae8bb0ce9b2ce1b8dac1a42
Tracey <radcliffe.tracey@gmail.com>
2016-08-01 10:46:04 -0700

    added router, errhandle from last lab, and coffee drink schema

--- lab-traceyr/gulpfile.js
+++ lab-traceyr/gulpfile.js
@@ -8,13 +8,13 @@ var allFiles = ['index.js', 'server.js', 'test/**/*.js', 'lib/**/*.js', 'gulpfil
 
 gulp.task('lint:all', () => {
   gulp.src(allFiles)
-  .pipe(eslint())
-  .pipe(eslint.format());
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
 
 gulp.task('mocha:test', () => {
   gulp.src(testFiles)
-  .pipe(mocha());
+    .pipe(mocha());
 });
 
 gulp.task('watch:files', () => {
--- lab-traceyr/route/router.js
+++ lab-traceyr/route/router.js
@@ -1,9 +1,15 @@
 'use strict';
 
+'use strict';
+
 const Router = require('express').Router;
-// const appErr = require('../mode/app_err_handle');
+const appErr = require('../model/app_err_handle');
+const Coffee = require('../model/coffee_const.js');
+
+const bodyParser = require('body-parser').json();
 
 let router = new Router();
+
 
 // router.post();
 //
